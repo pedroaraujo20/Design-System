64125384c4485a1d94ab59abb7b082e5c9860f94
Pedro Araujo <paraujo@avantstay.com>
2023-06-09 17:37:47 -0300

    fix: Add subpath to Storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -14,5 +14,12 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/Design-System/'
+    }
+    
+    return config
+  }
 };
 export default config;
