e8d97bd3ea9f39adfb2d7f26c82beab4bc34b7f3
Pedro Araujo <paraujo@avantstay.com>
2023-06-10 21:44:30 -0300

    fix: Add ref to TextInput and adjust styles

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -1,5 +1,10 @@
 import type { StoryObj, Meta } from '@storybook/react'
-import { Box, Text, TextInput, TextInputProps } from '@brightscript-ignite-ui/react'
+import {
+  Box,
+  Text,
+  TextInput,
+  TextInputProps,
+} from '@brightscript-ignite-ui/react'
 
 export default {
   title: 'Form/Text Input',
@@ -34,5 +39,6 @@ export const Disabled: StoryObj<TextInputProps> = {
 export const WithPrefix: StoryObj<TextInputProps> = {
   args: {
     prefix: 'cal.com/',
+    placeholder: 'your-username',
   },
 }
